be48e28c188a4dfd6696068740f65924738e1ca2
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-04-01 04:14:55 +0600

    cleaned up

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const admin = require("firebase-admin");
 const apiRouter = require("./routes/apiRoutes");
 
 const app = express();
